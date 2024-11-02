a3ad0398b69c57cae90c395c8ab195803548ac3f
Ecoin Finance <84532707+ecoin-finance@users.noreply.github.com>
2024-11-01 22:24:54 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -14,16 +14,5 @@ export default defineConfig({
     commonjs(),
     nodePolyfills()
   ],
-  build: {
-    rollupOptions: {
-      external: ['https://cdn.jsdelivr.net/npm/@ton/ton@15.1.0/dist/index.js'],
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
+
 });
